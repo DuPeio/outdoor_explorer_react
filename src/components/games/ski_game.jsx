b349ff5a37f52f71169ffdf78afdafdf525609f5
magieup <magieup@univ-grenoble-alpes.fr>
2026-05-19 12:54:57 +0200

    add canvas

--- src/components/games/ski_game.jsx
+++ src/components/games/ski_game.jsx
@@ -1,10 +1,14 @@
 function ski_game({setBook, setGame}){
     return (
-        <div className={"game-container"}>
+        <div className={"game"}>
             <button className={"back-button"} onClick={()=>{
                 setGame(false);
                 setBook(true);}}
             >Revenir au livre</button>
+
+            <canvas className="canvas">
+
+            </canvas>
         </div>
     )
 }
